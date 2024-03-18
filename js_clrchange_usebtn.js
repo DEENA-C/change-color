f7bdf242c0fd9c1123e11a955bc4186d2e4656c0
DEENA-C <163382006+DEENA-C@users.noreply.github.com>
2024-03-18 02:38:33 -0400

    created

---
+++ js_clrchange_usebtn.js
@@ -0,0 +1,21 @@
+        var box1=document.getElementById("box")
+        var box2=document.getElementById("square")
+        var box3=document.getElementById("shape")
+        var input=document.getElementById("demo")
+
+        function change()
+        {
+            input.style.backgroundColor="#00ffff"
+        }
+        function  color1()
+        {
+          box1.style.backgroundColor="red";
+        }
+        function color2()
+        {
+          box2.style.backgroundColor="pink";
+        }
+        function color3()
+        {  
+          box3.style.backgroundColor="yellow";
+        }
